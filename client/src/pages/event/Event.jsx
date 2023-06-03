085b75ae755a7c1ee080befd8da26167ab0f3944
Sunaira1101 <fahrin.sunaira@northsouth.edu>
2023-06-04 02:20:36 +0600

    event frontend done

--- client/src/pages/event/Event.jsx
+++ client/src/pages/event/Event.jsx
@@ -1,10 +1,60 @@
 import "./event.css"
-import React from 'react'
+import React, { useState } from 'react'
 
 const Event = () => {
+    const [cards] = useState([
+        {
+            title: 'International Autism Day',
+            pic: 'https://picsum.photos/200/300',
+            text: 'Annual April 2nd observance promoting autism awareness, acceptance, inclusion, support, and understanding worldwide, symbolized by the color blue.'
+        },
+        {
+            title: 'The Autism Show',
+            text: 'The Autism Show on April 2nd is an event focused on autism, offering information, resources, and support for individuals, families, and professionals.'
+        },
+        {
+            title: 'Inclusion Symposium',
+            text: 'On March 3rd, International School Dhaka (ISD) in Bangladesh will host the inaugural Inclusion Symposium, focusing on diversity and inclusion in child special needs education.'
+        },
+        {
+            title: 'Rise & Shine',
+            text: 'Rise & Shine on April 2nd is a special event dedicated to children with autism, providing a platform for them to shine and thrive, and showcase their abilities to the world.'
+        },
+        {
+            title: 'Inclusion Symposium',
+            text: 'On March 3rd, International School Dhaka (ISD) in Bangladesh will host the inaugural Inclusion Symposium, focusing on diversity and inclusion in child special needs education.'
+        },
+        {
+            title: 'Rise & Shine',
+            text: 'Rise & Shine on April 2nd is a special event dedicated to children with autism, providing a platform for them to shine and thrive, and showcase their abilities to the world.'
+        }
+    ])
+
+
+
   return (
-    <div>
-      Event
+    <div className="event">
+      <section>
+        <div className="eventcontainer">
+            <h1 className="eventname">Events</h1>
+            <div className="cards">
+                {
+                    cards.map((card, i) => (
+
+                        <div key={i} className="cardcard">
+                            <h3 className="cardname">{card.title}</h3>
+                            {/* <img className="piccard" src={card.pic} alt="" /> */}
+                            <span className="ptext">{card.text}</span>
+                            <button className="cardbtn">Explore</button>
+                        </div>
+
+                    ))
+                }
+                
+            </div>
+
+        </div>
+      </section>
     </div>
   )
 }
